363be571fb412d011336b74baf25a40fa055ebc4
cignare3 <50918984+cignare3@users.noreply.github.com>
2019-06-17 21:30:21 -0400

    Use pandas more

--- omniparser/gradebook_parser.py
+++ omniparser/gradebook_parser.py
@@ -1,2 +1,33 @@
 
-print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+import pandas
+import os
+import statistics
+
+def calculate_average_grade_from_csv(my_csv_filepath):
+    df = pandas.read_csv(my_csv_filepath) 
+    #breakpoint()
+
+    #rows = df.to_dict("records")
+    #grades = [r["final_grade"] for r in rows]
+    #avg_grade = statistics.mean(grades)
+
+    #grades = df["final_grade"].to_list()
+    #avg_grade = statistics.mean(grades)
+    
+    avg_grade = df["final_grade"].mean()
+
+    return avg_grade #90.64
+    
+
+if __name__ == "__main__":
+    print("PARSING SOME EXAMPLE GRADEBOOK FILES HERE...")
+
+    gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "gradebook_2019.csv")
+    print(gradebook_filepath)
+    
+    #gradebook_filepath = "C::\Users\tyler\Documents\GitHub\omniparser-starter-py\data\gradebook_2018.csv"
+    
+    #gradebook_filepath = "data\gradebook_2018.csv"
+    #breakpoint()
+    avg = calculate_average_grade_from_csv(gradebook_filepath)
+    print(avg)
